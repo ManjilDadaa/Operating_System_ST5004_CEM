d70cf6c24de884f6c551f7382d67031496486753
ManjilDadaa <dev.manjil.c@gmail.com>
2026-06-27 20:03:44 +0545

    chore: Import library files and define constants

---
+++ Task1/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<unistd.h>
+#include<string.h>
+
+// Constants
+
+#define NUM_THREADS 3
+#define TIME_SLICE 1
+#define TOTAL_WORK 3
+
